7a5fd6232e4ed117b694bc48c151a08155da208f
olawale-o <omoogunolawale@gmail.com>
2022-05-24 18:28:54 +0100

    Create private view

---
+++ src/components/Private/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <HomeComponent />
+</template>
+
+<script>
+import HomeComponent from '@/components/Private/Home.vue'
+export default {
+  name: 'AuthView',
+  components: {
+    HomeComponent,
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
