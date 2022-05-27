11fbbebb0e64120f629fbca47586f3b63d8178c3
olawale-o <omoogunolawale@gmail.com>
2022-05-27 01:00:45 +0100

    Create login view

---
+++ src/views/Auth/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="auth">
+    <LoginComponent />
+  </div>
+</template>
+
+<script>
+import LoginComponent from '@/components/Auth/Login.vue'
+export default {
+  name: 'LoginView',
+  components: {
+    LoginComponent,
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+</style>
