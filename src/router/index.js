8a7db9f19c56469c137b0f5af0f6bfc274d0c920
olawale-o <omoogunolawale@gmail.com>
2022-05-27 01:02:09 +0100

    Add forgot password route

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,33 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import AuthView from '../views/Auth'
+import { createRouter, createWebHistory } from 'vue-router';
+import AuthView from '../views/Auth';
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: AuthView
+  },
+  {
+    path: '/auth',
+    name: 'auth',
+    component: AuthView,
+    children: [
+      {
+        path: '',
+        name: 'index',
+        redirect: { name: 'Login' },
+      },
+      {
+        path: 'login_in',
+        name: 'Login',
+        component: () => import('../views/Auth/Login.vue'),
+      },
+      {
+        path: 'forgot_password',
+        name: 'ForgotPassword',
+        component: () => import('../views/Auth/ForgotPassword.vue'),
+      },
+    ],
   },
   {
     path: '/private',
