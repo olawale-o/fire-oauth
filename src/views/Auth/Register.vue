d4337fbe8cde4e38c613a78bf7af3834575141f7
olawale-o <omoogunolawale@gmail.com>
2022-05-30 16:16:43 +0100

    Create register view

---
+++ src/views/Auth/Register.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="auth">
+    <RegisterComponent />
+  </div>
+</template>
+
+<script>
+import RegisterComponent from '@/components/Auth/Register.vue'
+export default {
+  name: 'RegisterView',
+  components: {
+    RegisterComponent,
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+</style>
